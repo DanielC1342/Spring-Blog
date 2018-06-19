7bf4c0a0eb2f14caeed2f7ec8f390a1fdc64afbd
danielconner <metalman1342@gmail.com>
2018-06-19 10:05:41 -0500

    added some post functionallity

--- src/main/java/com/codeup/blog/PostController.java
+++ src/main/java/com/codeup/blog/PostController.java
@@ -1,21 +1,34 @@
 package com.codeup.blog;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+
 @Controller
 public class PostController {
 
     @GetMapping("/posts")
-    public @ResponseBody String postIndex() {
-        return "Here is the post index";
+    public String postIndex(Model model) {
+        Post postA = new Post("This is post A","Here is the body of post A");
+        Post postB = new Post("This is post B","Here is the body of post B");
+        Post postC = new Post("This is post C","Here is the body of post C");
+        ArrayList<Post> list = new ArrayList<>();
+        list.add(postA);
+        list.add(postB);
+        list.add(postC);
+        model.addAttribute("list",list);
+        return "posts/index";
     }
     @GetMapping("/posts/{id}")
-    public @ResponseBody String postIndexId(@PathVariable int id) {
-        return String.format("Here is the post with the ID of %d", id);
+    public String postIndexId(@PathVariable int id, Model model) {
+        Post post = new Post("This is a Post","Here is the body of the post");
+        model.addAttribute("post",post);
+        return "posts/show";
     }
     @GetMapping("/posts/create")
     public @ResponseBody String createPost() {
