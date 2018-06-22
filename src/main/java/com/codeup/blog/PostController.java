82b913f8ff01cffafbc49d3c23be1321ede4c689
danielconner <metalman1342@gmail.com>
2018-06-22 11:13:25 -0500

    edit function setup but not implemented properly

--- src/main/java/com/codeup/blog/PostController.java
+++ src/main/java/com/codeup/blog/PostController.java
@@ -7,31 +7,50 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 
 @Controller
-public class PostController {
-    private final PostService postsvc;
+class PostController {
+    private PostRepo postDao;
 
-    public PostController(PostService postsvc) {
-        this.postsvc = postsvc;
+    public PostController(PostService postsvc, PostRepo postDao) {
+        this.postDao = postDao;
     }
 
     @GetMapping("/posts")
     public String postIndex(Model model) {
-        model.addAttribute("list",postsvc.all());
+        model.addAttribute("list",postDao.findAll());
         return "posts/index";
     }
     @GetMapping("/posts/{id}")
     public String postIndexId(@PathVariable int id, Model model) {
-        model.addAttribute("post",postsvc.find(id));
+        model.addAttribute("post",postDao.findOne(id));
         return "posts/show";
     }
     @GetMapping("/posts/{id}/edit")
     public String editPost(@PathVariable int id, Model model) {
-        Post edit = postsvc.find(id);
+        Post edit = postDao.findOne(id);
         model.addAttribute("post", edit);
         model.addAttribute("title",edit.getTitle());
         model.addAttribute("body",edit.getBody());
         return "posts/edit";
     }
+
+    @PostMapping("/posts/{id}/edit")
+    public String updatePost(
+            @PathVariable int id,
+            @RequestParam(name = "nuTitle") String title,
+            @RequestParam(name = "nuBody") String body
+    ) {
+        //some kind of hibernate update query
+        postDao.save(postDao.findOne(id));
+        return "redirect:/posts";
+    }
+
+    @PostMapping("/posts/{id}/delete")
+    public String deletePost(@PathVariable int id) {
+        Post foo = postDao.findOne(id);
+        postDao.delete(foo);
+        return "redirect:/posts";
+    }
+
 
     @GetMapping("/posts/create")
     public String createPost(Model model) {
@@ -45,7 +64,7 @@ public class PostController {
         @RequestParam(name = "body") String body
     ) {
         Post post = new Post(0,title,body);
-        postsvc.save(post);
+        postDao.save(post);
         return "redirect:/posts";
     }
 }
